4f500abfd6b0c11cd89801ebf74ccacffa3e659e
Andreas Fischer <andreas.fischer@balanceofcowards.net>
2013-09-10 15:17:52 +0200

    Small changes; translation to english.

--- modloader.py
+++ modloader.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 import sys, os, logging
-from os.path import splitext, join
+from os.path import splitext, join, isabs
 
-ERR_IMPORT = "Dateiimport von %s%s fehlgeschlagen."
-ERR_MISSINGFUNC = "Modul %s hat keine %s-Funktion. Import abgebrochen."
+ERR_IMPORT = "File import of module %s%s has failed."
+ERR_MISSINGFUNC = "Module %s lacks function '%s'. Import aborted."
 LOGGER = logging.getLogger(__name__)
 
 def get_modules(directory, function):
@@ -12,35 +12,41 @@ def get_modules(directory, function):
 
         TODO: Prüfen, dass 'directory' existiert
         TODO: Mehrere Funktionen unterstützen
+        TODO: Klassen unterstützen
+        TODO: I18N, L10N
+        TODO: Use inspect module
     """
-    # Das Verzeichnis zum PYTHONPATH hinzufügen
-    sys.path.append(join(os.getcwd(), directory))
+    # Add directory to PYTHONPATH
+    if isabs(directory):
+        sys.path.append(directory)
+    else:
+        sys.path.append(join(os.getcwd(), directory))
 
-    # Zerteile die Dateinamen in Basisname und Erweiterung
+    # Create a list of files split into basename and extension
     modulefiles = [splitext(mfile) for mfile in os.listdir(directory)]
 
-    # Filtere alle Dateien, die nicht auf ".py" enden aus der Liste
+    # Filter for files ending in '.py'
     modulefiles = [(mfile, ext) for (mfile, ext) in modulefiles if ext == ".py"]
 
     modlist = []
     for (modfile, ext) in modulefiles:
-        try: # Versuche Import
+        try: # Try to import
             mod_import = __import__(modfile)
         except ImportError:
             LOGGER.error(ERR_IMPORT % (modfile, ext))
             LOGGER.error(sys.exc_info())
         else:
             if (modfile == '__init__'):
-                LOGGER.info('__init__.py ignoriert')
-            # Nur Module mit oben übergebener Funktion werden akzeptiert
+                LOGGER.info("Ignore '__init__.py'.")
+            # Only accept modules with given function
             elif (hasattr(mod_import, function)):
                 if (hasattr(mod_import, "NOEXEC")):
-                    if (mod_import.NOEXEC): # NOEXEC? -> Modul auslassen
-                        LOGGER.info("Modul %s ausgelassen" \
+                    if (mod_import.NOEXEC): # NOEXEC? -> Skip module
+                        LOGGER.info("Skip module %s." \
                                                 % mod_import.__name__)
                         continue
                 modlist.append(mod_import)
-                LOGGER.info('Modul %s aus %s%s importiert.' \
+                LOGGER.info('Imported module %s from %s%s.' \
                                         % (mod_import.__name__, modfile, ext))
             else:
                 LOGGER.warning(ERR_MISSINGFUNC % (modfile, function))
