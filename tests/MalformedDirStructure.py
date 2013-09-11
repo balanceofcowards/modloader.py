e519a225f41ab30abbe434c2834d3da707b81bb8
Andreas Fischer <andreas.fischer@balanceofcowards.net>
2013-09-11 13:54:59 +0200

    Started to write unit tests

---
+++ tests/MalformedDirStructure.py
@@ -0,0 +1,24 @@
+import unittest
+import modloader
+
+class TestEmptyDir(unittest.TestCase):
+    def setUp(self):
+        """
+            TODO: Create temporary directory and file structure.
+        """
+        pass
+
+    def test_empty_dir(self):
+        """
+            TODO: write function
+        """
+        pass
+
+    def tearDown(self):
+        """
+            TODO: Remove temporary files again.
+        """
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
